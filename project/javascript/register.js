56ea88c58a4a9656f4317a511aca07baf1390590
Samya-ab <sa2306611@qu.edu.qa>
2026-03-26 07:52:11 +0300

    fixing small mistake

--- project/javascript/register.js
+++ project/javascript/register.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", function(e) {
     const username = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
-    const fileInput=document.getElementById('Profile-picture');
+    const fileInput=document.getElementById('profile');
     const file=fileInput.files[0];
     let users = JSON.parse(localStorage.getItem("users")) || [];
 
