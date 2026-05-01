d04444d0007179a1d0f1527da1ef142ef5de9e2c
Samya-ab <sa2306611@qu.edu.qa>
2026-05-01 20:22:22 +0300

    integrating api in phase 1 pt1

---
+++ MURMUR/murmur-phase2/app/public/Phase1/javascript/api.js
@@ -0,0 +1,18 @@
+async function api(path, options = {}) {
+  const res = await fetch(path, {
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+    ...options,
+  });
+
+  const data = await res.json().catch(() => null);
+
+  if (!res.ok) {
+    throw new Error(data?.error || "Something went wrong");
+  }
+
+  return data;
+}
